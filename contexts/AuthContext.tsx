72e160d404a260b23202906cd39e3ff8b77426d9
Carlos Cenci <carlosc.estevam@gmail.com>
2021-09-30 11:37:41 -0300

    Finish Authentication with SSR

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,7 +1,7 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
 import { setCookie, parseCookies, destroyCookie } from 'nookies'
 import Router from 'next/router'
-import { api } from "../services/api";
+import { api } from "../services/apiClient";
 
 type User = {
     email: string;
@@ -85,6 +85,7 @@ export function AuthProvider({ children } : AuthProviderProps) {
     }
 
     return (
+         // @ts-ignore
         <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
             {children}
         </AuthContext.Provider>
